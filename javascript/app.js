1b16e2d376c56ca4f35e82d7f8e5cb28f9cb7d02
jimenezbr7 <jimenezbr7@gmail.com>
2017-12-07 20:29:35 -0500

    adding css animations

--- javascript/app.js
+++ javascript/app.js
@@ -1,18 +1,20 @@
 $('nav').onePageNav();
 
 $('#nav-btn').click(function() {
-  $('nav').slideToggle();
+  $('nav').slideToggle(1000);
+  $('body').toggleClass('padded');
+  // $('#home').animate({paddingTop: '250px'}, 5000);
 });
 
 var nav = $('nav');
 
 // When the user resizes the window
-$(window).resize(function(){
+$(window).resize(function() {
   // Get the width of the page and store it in a nav
-   var width = $(window).width();
-   // If width > breakpoint and nav is hidden
-   if(width > 768 && nav.is(':hidden')) {
-     // Change the css of the nav to display blank
-       nav.css('display', '');
-   }
+  var width = $(window).width();
+  // If width > breakpoint and nav is hidden
+  if (width > 768 && nav.is(':hidden')) {
+    // Change the css of the nav to display blank
+    nav.css('display', '');
+  }
 });
